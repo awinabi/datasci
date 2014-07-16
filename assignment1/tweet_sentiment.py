883bf094619ff77ca45342f20d4eaedb5d78794f
Awin Abi <awinabi@gmail.com>
2014-07-16 23:20:34 +0530

    sentiment score of a tweet

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,40 @@
 import sys
+import json as JSON
+
+scores = {}
 
 def hw():
-    print 'Hello, world!'
+  print "hello world!"
 
 def lines(fp):
-    print str(len(fp.readlines()))
+  print str(len(fp.readlines()))
+
+def parse_afinn(fp):
+  fp.seek(0,0)
+  for line in fp:
+    term, score  = line.split("\t")
+    scores[term] = int(score)
+
+def tweet_score(tweet):
+  tweetid = tweet.get('id', 'NULL')
+  tweet_text = tweet.get('text', '')
+  wscores = [scores.get(word, 0) for word in tweet_text.split()]
+  tweet_score = sum(wscores)
+  print tweetid, tweet_score
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+  afile = open(sys.argv[1])
+  tfile = open(sys.argv[2])
+  hw()
+  lines(afile)
+  lines(tfile)
+
+  parse_afinn(afile)
+
+  tfile.seek(0,0)
+  for line in tfile:
+    tweet = JSON.loads(line)
+    tweet_score(tweet)
 
 if __name__ == '__main__':
-    main()
+  main()
